dc2c96d196df38c282950c984eabfa270a5b1047
Al Siam <mdmahedialsiam@gmail.com>
2021-11-24 06:08:57 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,50 +1,33 @@
-import React  from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
 import OrderReview from './components/OrderReview/OrderReview';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Register from './components/Register/Register';
 import Shop from './components/Shop/Shop';
 import TheNotFound from './components/TheNotFound/NotFoun';
+import AuthProvider from './context/AuthProvider';
 
 function App() {
-  console.log('hello')
   return (
-    <div className="App">
-      <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/">
-            <Shop></Shop>
-          </Route>
-          <Route exact path="/login">
-            <Login></Login>
-          </Route>
-          <Route exact path="/register">
-            <Register></Register>
-          </Route>
-          <Route path="/shop">
-            <Shop></Shop>
-          </Route>
-          <Route path="/orders">
-            <OrderReview></OrderReview>
-          </Route>
-          <Route path="/inventory">
-            <Inventory></Inventory>
-          </Route>
-          <Route path="/orderplaced">
-            <PlaceOrder></PlaceOrder>
-          </Route>
-          <Route path="*">
-            <TheNotFound></TheNotFound>
-          </Route>
-        </Switch>
-      </Router>
+    <AuthProvider>
+      <Header></Header>
+      <Routes>
+        <Route exact path="/" element={<Shop />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/orders" element={<OrderReview />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <PrivateRoute path="/orderplaced" element={<PlaceOrder />} />
+        <Route path="*" element={<TheNotFound />} />
+      </Routes>
 
-    </div>
+    </AuthProvider>
   );
 }
 
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,10 +1,12 @@
 import React, { createContext } from 'react';
+import useFirebase from '../Hooks/useFirebase';
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
+    const auth=useFirebase();
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={auth}>
             {children}
         </AuthContext.Provider>
     );
